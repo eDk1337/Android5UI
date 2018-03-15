6b20ca287f79a45b8ee7e63c444855e8f1d16dd2
edk1337 <edvinas211@gmail.com>
2018-03-15 16:45:34 +0000

    added Floating Action Button and added Navigation Drawer

--- app/src/main/java/com/example/a2ramae45/android5ui/MainActivity.java
+++ app/src/main/java/com/example/a2ramae45/android5ui/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a2ramae45.android5ui;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
 
+        NavigationView nView = (NavigationView)findViewById(R.id.navigationView);
+        final DrawerLayout dLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        nView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            public boolean onNavigationItemSelected(MenuItem item) {
+                String what = "";
+                switch (item.getItemId()) {
+                    case R.id.save:
+                        what = "SAVE";
+                        break;
+
+                    case R.id.load:
+                        what = "LOAD";
+                        break;
+                }
+                new AlertDialog.Builder(MainActivity.this).setPositiveButton("OK", null).setMessage(what).show();
+                dLayout.closeDrawers();
+                return true;
+
+            }
+
+    });
     }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -28,8 +55,21 @@ public class MainActivity extends AppCompatActivity {
         MenuItem item = menu.findItem(R.id.search);
         SearchView sv = (SearchView) MenuItemCompat.getActionView(item);
         sv.setOnQueryTextListener(new SearchHandler());
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab1);
+        fab.setOnClickListener (new View.OnClickListener() {
+
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setPositiveButton("OK", null).
+                        setMessage("The FloatingActionButton was clicked!").show();
+            }
+        });
         return true;
+
     }
+
+
 
     public  boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == R.id.save) {
@@ -37,8 +77,16 @@ public class MainActivity extends AppCompatActivity {
                     setMessage("Saving...").show();
             return true;
         }
+
+        if(item.getItemId() == R.id.load) {
+            new AlertDialog.Builder(this).setPositiveButton("OK", null).setMessage("Settings/Load...").show();
+        }
+
+
         return false;
     }
+
+
 
     class SearchHandler implements SearchView.OnQueryTextListener {
 
@@ -53,6 +101,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
+
+
 
 
 }
